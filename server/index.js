1fccc886ffea494fc98e3adfee742c2624322115
Fred Nyambati <Fred@Admins-MacBook-Pro-3.local>
2016-03-21 17:09:02 -0600

    good start

--- server/index.js
+++ server/index.js
@@ -7,3 +7,18 @@ var session = require('express-session');
 
 /////////FILES//////////////
 var passport = require('./services/passport.js');
+var config = require('./config');
+
+var app = express();
+
+//CONNECTIONS TO MONGO AND PORT /////////
+var mongoURI = config.MONGO_URI;
+var port = config.PORT;
+mongoose.connect(mongoURI);
+mongoose.connection.once('open', function() {
+  console.log('POWER LEVEL OVER ' + port + ' GIVE ME YOUR ENERGY');
+  app.listen(port, function() {
+    console.log("SPIRIT BOMB READY!");
+
+  });
+});
